0a0b13f6e282703b9a1d10af1a8a1e2ec41b08c6
Earth Patel <earthrpatel@gmail.com>
2025-12-21 19:49:14 +0530

    feat: implement user authentication with bcrypt for login and registration

--- project-management/server/src/controllers/auth.controller.ts
+++ project-management/server/src/controllers/auth.controller.ts
@@ -1,6 +1,44 @@
 import type { Request, Response } from 'express';
 
+import bcrypt from 'bcrypt';
+
 import { createUser } from '../services/auth.service';
+import { loginUser } from '../services/auth.service';
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  try {
+    // fetch user from DB
+    const user = await loginUser(email);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // compare password with hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // success
+    res.status(200).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
 export const register = async (req: Request, res: Response) => {
   console.log(req.body);
--- project-management/server/src/services/auth.service.ts
+++ project-management/server/src/services/auth.service.ts
@@ -1,3 +1,5 @@
+import bycrypt from "bcrypt";
+
 import prisma from "../prisma";
 
 interface CreateUserInput {
@@ -6,9 +8,19 @@ interface CreateUserInput {
   name: string;
 }
 
+// bcrypt will run the hashing algorithm 2¹⁰ ≈ 1024 times internally
+const SALT_ROUNDS = 10;
+
 export const createUser = async (data: CreateUserInput) => {
+  // Hash the password before storing it
+  const hasedPassword = await bycrypt.hash(data.password, SALT_ROUNDS);
+
   return prisma.user.create({
-    data,
+    data: {
+      email: data.email,
+      name: data.name,
+      password: hasedPassword,
+    },
     select: {
       id: true,
       name: true,
@@ -18,3 +30,16 @@ export const createUser = async (data: CreateUserInput) => {
     }
   })
 };
+
+export const loginUser = async (email: string) => {
+  return prisma.user.findUnique({
+    where: { email },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      password: true,
+    }
+  });
+}
