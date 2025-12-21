8a7a671786163b356c6e2405ef2abb7be4690032
Earth Patel <earthrpatel@gmail.com>
2025-12-21 21:22:10 +0530

    feat: add JWT authentication with jsonwebtoken and implement auth middleware

--- project-management/server/src/controllers/auth.controller.ts
+++ project-management/server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express';
 
 import bcrypt from 'bcrypt';
+import jwt, { type SignOptions } from 'jsonwebtoken';
 
 import { createUser } from '../services/auth.service';
 import { loginUser } from '../services/auth.service';
@@ -27,12 +28,26 @@ export const login = async (req: Request, res: Response) => {
       return res.status(401).json({ message: 'Invalid password' });
     }
 
+    if (!process.env.JWT_SECRET || !process.env.JWT_EXPIRES_IN) {
+      throw new Error("JWT_SECRET or JWT_EXPIRES_IN is not defined");
+    }
+
+    // generate JWT token
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN } as SignOptions
+    );
+
     // success
     res.status(200).json({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      }
     });
   } catch (error) {
     console.error(error);
--- project-management/server/src/routes/auth.route.ts
+++ project-management/server/src/routes/auth.route.ts
@@ -1,10 +1,19 @@
-import { Router } from 'express';
+import { Router, type Response } from 'express';
 
 import { register, login } from '../controllers/auth.controller';
+import { authMiddleware, type AuthRequest } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+// currently a test route to verify auth middleware
+router.get('/me', authMiddleware, (req: AuthRequest, res: Response) => {
+  res.json({
+    message: 'You are authenticated',
+    user: req.user
+  });
+});
 
 export default router;
