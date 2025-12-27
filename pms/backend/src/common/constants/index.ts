ed6cc116caec678ec0c43c1b1be6a3f6ed3b0388
Earth Patel <earthrpatel@gmail.com>
2025-12-27 18:35:43 +0530

    feat: initialize backend structure with TypeScript, Express, and essential configurations

---
+++ pms/backend/src/common/constants/index.ts
@@ -0,0 +1,10 @@
+/******************************************************************************
+                              Constants
+******************************************************************************/
+
+// NOTE: These need to match the names of your ".env" files
+export const NODE_ENVS = {
+  Dev: 'development',
+  Test: 'test',
+  Production: 'production'
+} as const;
