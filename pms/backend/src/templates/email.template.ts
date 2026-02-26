0484dc8341b17ef3e5f929e0ca6c207d61b4d633
Earth Patel <earthrpatel@gmail.com>
2026-02-26 22:57:23 +0530

    fix: remove trailing whitespace in reset password email template

--- pms/backend/src/templates/email.template.ts
+++ pms/backend/src/templates/email.template.ts
@@ -24,7 +24,7 @@ export const sendResetPasswordEmail = async (to: string, token: string) => {
       <h2>Password Reset</h2>
       <p>Click the link below to reset your password:</p>
       <a href='${resetUrl}'>${resetUrl}</a>
-      <p>This link will expire in 15 minutes.</p>  
+      <p>This link will expire in 15 minutes.</p>
     `
   });
 };
