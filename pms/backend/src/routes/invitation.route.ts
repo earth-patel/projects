102a4ad7df43a7cd06a1a7fd44ec0c0862585630
Earth Patel <earthrpatel@gmail.com>
2026-03-15 19:01:35 +0530

    feat: add route to retrieve invitation information

--- pms/backend/src/routes/invitation.route.ts
+++ pms/backend/src/routes/invitation.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
-import { invite } from '../controllers/invitation.controller';
+import {
+  getInvitationInfo,
+  invite
+} from '../controllers/invitation.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/send-invite', authMiddleware, invite);
+router.get('/info', getInvitationInfo);
 export default router;
