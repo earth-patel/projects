f78cab5ecefd06b06b72e58b49ebf6b08b3f2c9e
Earth Patel <earthrpatel@gmail.com>
2026-03-30 22:53:30 +0530

    feat: add ProjectItem and ProjectState interfaces for project management

---
+++ pms/frontend/src/store/project/project.types.ts
@@ -0,0 +1,18 @@
+import { type ProjectApiErrorResponse } from "../../types/api.types";
+
+export interface ProjectItem {
+  id: number;
+  name: string;
+  description?: string;
+  createdBy: { id: number; firstName: string; lastName: string };
+  createdAt: string;
+  _count: { tasks: number };
+}
+
+export interface ProjectState {
+  projects: ProjectItem[];
+  projectsLoading: boolean;
+  projectsError: ProjectApiErrorResponse | null;
+  createProjectLoading: boolean;
+  deleteProjectLoading: boolean;
+}
